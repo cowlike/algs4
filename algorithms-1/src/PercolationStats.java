cfead32a8a2fc3963c336d709aa941a7c832451f
cowlike <cowlike@gmail.com>
2013-09-02 13:35:31 -0500

    added code for percolation problem

--- algorithms-1/src/PercolationStats.java
+++ algorithms-1/src/PercolationStats.java
@@ -1,42 +1,78 @@
 public class PercolationStats {
+	private final int gridSize;
+	private final int nTests;
+	private final double[] results;
+
 	/*
 	 * perform T independent computational experiments on an N-by-N grid
 	 */
-	public PercolationStats(final int N, final int T){
+	public PercolationStats(final int N, final int T) {
 		if (N <= 0 || T <= 0) {
 			throw new IllegalArgumentException("N and T must be greater than 0");
 		}
+
+		gridSize = N;
+		nTests = T;
+		results = new double[nTests];
 	}
 
 	/*
 	 * sample mean of percolation threshold
 	 */
 	public double mean() {
-		return 0;
+		return StdStats.mean(results);
 	}
 
 	/*
 	 * sample standard deviation of percolation threshold
 	 */
 	public double stddev() {
-		return 0;
+		return StdStats.stddev(results);
 	}
 
 	/*
 	 * returns lower bound of the 95% confidence interval
 	 */
 	public double confidenceLo() {
-		return 0;
+		return mean() - (1.96 * stddev() / Math.sqrt(nTests));
 	}
 
 	/*
 	 * returns upper bound of the 95% confidence interval
 	 */
 	public double confidenceHi() {
-		return 0;
+		return mean() + (1.96 * stddev() / Math.sqrt(nTests));
+	}
+
+	public void run() {
+		// execute the tests
+		for (int n = 0; n < nTests; n++) {
+			int threshold = 0;
+			Percolation p = new Percolation(gridSize);
+
+			while (!p.percolates()) {
+				int i = StdRandom.uniform(gridSize) + 1;
+				int j = StdRandom.uniform(gridSize) + 1;
+
+				if (!p.isOpen(i, j)) {
+					p.open(i, j);
+					threshold++;
+				}
+			}
+			results[n] = ((double)threshold) / (gridSize * gridSize);
+		}
 	}
 
 	public static void main(String[] args) {
-		// run tests
+		final int gridSize = StdIn.readInt();
+		final int tests = StdIn.readInt();
+
+		PercolationStats s = new PercolationStats(gridSize, tests);
+		s.run();
+
+		StdOut.printf("%-23s = %f\n", "mean", s.mean());
+		StdOut.printf("%-23s = %f\n", "stddev", s.stddev());
+		StdOut.printf("%-23s = %f, %f\n", "95% confidence interval",
+				s.confidenceLo(), s.confidenceHi());
 	}
 }
