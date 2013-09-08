99b2463f7f795236f8b58ffbb118f89db22c9d00
cowlike <cowlike@gmail.com>
2013-09-08 18:46:46 -0500

    skeleton classes for queues assignment

---
+++ algorithms-1/src/Deque.java
@@ -0,0 +1,40 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+    public Deque() {
+        // construct an empty deque
+    }
+
+    public boolean isEmpty() {
+        // is the deque empty?
+        return true;
+    }
+
+    public int size() {
+        // return the number of items on the deque
+        return 0;
+    }
+
+    public void addFirst(Item item) {
+        // insert the item at the front
+    }
+
+    public void addLast(Item item) {
+        // insert the item at the end
+    }
+
+    public Item removeFirst() {
+        // delete and return the item at the front
+        return null;
+    }
+
+    public Item removeLast() {
+        // delete and return the item at the end
+        return null;
+    }
+
+    public Iterator<Item> iterator() {
+        // return an iterator over items in order from front to end
+        return null;
+    }
+}
